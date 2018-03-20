77e35217020e652b7f9a3b91b868b5d5331fa38a
mikelhsia <mikelhsia@hotmail.com>
2018-03-21 01:28:33 +0800

    [PHP Laravel] 1. Create a Task Controller to control all tasks 2. Switch all functionalities from model to Controller

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@
 
 use App\Task;
 
+/***********************************************
+// Route to controller instead of inline function
+************************************************/
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
+
+
+
+/***********************************************
+// Route without controller
+************************************************/
 Route::get('/', function () {
 	/* Method 1 */
     // return view('welcome', [
@@ -36,30 +47,23 @@ Route::get('/', function () {
 
 });
 
-Route::get('/tasks', function () {
-    /* Method 5: Query database using Laravel's query sentence */
-    //$tasks = DB::table('tasks')->latest()->get();
-
-    // If I use App/Task, then I can skip the namespace
-    $tasks = Task::all(); // Eloquent method
-
-    // return $tasks; // Return data as JSON
-    return view("tasks.index", compact("tasks"));
-
-});
+// Route::get('/tasks', function () {
+//     /* Method 5: Query database using Laravel's query sentence */
+//     //$tasks = DB::table('tasks')->latest()->get();
+//     // If I use App/Task, then I can skip the namespace
+//     $tasks = Task::all(); // Eloquent method
+//     // return $tasks; // Return data as JSON
+//     return view("tasks.index", compact("tasks"));
+// });
 
 /* Wild cart route */
-Route::get('/tasks/{id}', function ($id) {
-	/* Helper function */
-	// dd($id);
-
-    /* Method 6: Query database using Laravel's query sentence assistance*/
-    // $task = DB::table('tasks')->find($id);
-    $task = App\Task::find($id); // Eloquent method
-
-    // dd($task);
-    
-    return view("tasks.show", compact("task"));
-
-});
+// Route::get('/tasks/{id}', function ($id) {
+// 	/* Helper function */
+// 	// dd($id);
+//     /* Method 6: Query database using Laravel's query sentence assistance*/
+//     // $task = DB::table('tasks')->find($id);
+//     $task = App\Task::find($id); // Eloquent method
+//     // dd($task);
+//     return view("tasks.show", compact("task"));
+// });
 
