ecadc902dccfe5cf0ebbd47dee0032dba7243983
mikelhsia <mikelhsia@hotmail.com>
2018-03-23 00:43:17 +0800

    [PHP Laravel] 1. Establish Comment model/controller/migration 2. Establish Comment-Post relationship

--- app/Post.php
+++ app/Post.php
@@ -3,10 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Comment;
 
 class Post extends Model
 {
     // construct fillable format to accept the data that valid by internal controller
     // PostsController.php
     protected $fillable = ['title', 'body'];
+
+    public function comments() {
+    	// return $this->hasMany('App\Comment');
+    	return $this->hasMany(Comment::class);
+    }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,6 +4,19 @@
 	<div class="title">{{ $post->title }}</div>
 	<div class="body">{{ $post->body }}</div>
 	<hr>
+	<div class="comment">
+		<ul>
+			@foreach ($post->comments as $comment)
+				<li style="width:100%; border:1px solid grey; list-style-type:none;">
+					<article>
+						<strong>{{ $comment->created_at->diffForHumans()}}</strong> <br>
+						{{ $comment->body }}
+					</article>
+				</li>
+			@endforeach
+		</ul>
+	</div>
+	<br>
 	<div style="margin-left:400px"><a href="/posts">Back to Catalog</a></div>
 @endsection
 
