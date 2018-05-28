c2e0db61251d9dbbe96920aa9f5378733a469336
mikelhsia <mikelhsia@hotmail.com>
2018-05-28 22:52:32 +0800

    [Vue + Laravel app] Remove and move file to folders

--- resources/views/layouts/errors.blade.php
+++
@@ -1,9 +0,0 @@
-@if (count($errors))
-    <div class='alert alert-danger' style="background:pink; border: 1px solid red; border-radius: 2px;">
-        <ul>
-            @foreach($errors->all() as $error):
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
